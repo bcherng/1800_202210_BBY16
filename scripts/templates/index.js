945a4d60d46c2102b7a6605e2179779317da592e
Simrat Grewal <simrats169169@gmail.com>
2022-02-28 20:16:00 -0800

    templates/index.js: Remove debug print

--- scripts/templates/index.js
+++ scripts/templates/index.js
@@ -3,7 +3,6 @@ import { logOut } from "../index/login.js";
 
 readFileAndSetHtml("nav-placeholder", "/templates/nav.html").then(() => {
   document.getElementById("log-out").addEventListener("click", (e) => {
-    console.log("here2");
     logOut();
     window.location.href = "/";
   });
