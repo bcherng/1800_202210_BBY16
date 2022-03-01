d228c31b25851d8a78c11e4f4cc0e974920112a1
Simrat Grewal <simrats169169@gmail.com>
2022-02-28 20:17:46 -0800

    feat(nav): Only show log out button for logged in users

--- scripts/templates/index.js
+++ scripts/templates/index.js
@@ -1,11 +1,16 @@
 import { readFileAndSetHtml } from "./readfile.js";
-import { logOut } from "../index/login.js";
+import { isLoggedIn, logOut } from "../index/login.js";
 
 readFileAndSetHtml("nav-placeholder", "/templates/nav.html").then(() => {
-  document.getElementById("log-out").addEventListener("click", (e) => {
-    logOut();
-    window.location.href = "/";
-  });
+  const logOutButton = document.getElementById("log-out");
+  if (!isLoggedIn()) {
+    logOutButton.style.display = "none";
+  } else {
+    logOutButton.addEventListener("click", (e) => {
+      logOut();
+      window.location.href = "/";
+    });
+  }
 });
 
 readFileAndSetHtml("footer-placeholder", "/templates/footer.html");
