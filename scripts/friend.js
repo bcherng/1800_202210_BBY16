cdb020f4bd7e6173bc35d4be2925006b6d3fbfe1
Sumit <veerkhanduri@gmail.com>
2022-03-09 21:30:22 -0800

    made friend.js but the button needs some working on

---
+++ scripts/friend.js
@@ -0,0 +1,8 @@
+import { readFileAndSetHtml } from "./templates/readfile";
+
+
+let doc = document.getElementById("addFriends");
+doc.addEventListener("click", () => {
+    readFileAndSetHtml("friendSearchBar", "/templates/searchbar.html.txt");
+
+})
