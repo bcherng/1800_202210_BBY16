76d34bcf8912cd50497ab93312fbc8941bbbac4a
Simrat Grewal <simrats169169@gmail.com>
2022-03-30 13:41:38 -0700

    feat(profile): Add past predictions from database

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,27 +1,41 @@
-
-import { db } from "./firebaseAPI_TEAM_BBY_16.js"
+import { db } from "./firebaseAPI_TEAM_BBY_16.js";
 import { doc, getDoc } from "firebase/firestore";
+import { getID } from "./index/login.js";
 
-const storage = window.localStorage;
-const token = storage.getItem("token");
+function main() {
+  const docRef = doc(db, "users", getID());
+  getDoc(docRef).then((snap) => {
+    if (snap.exists()) {
+      const data = snap.data();
+      document.getElementById("userRankPlaceHolder").innerHTML =
+        data.leaderboardPos;
+      document.getElementById("userNamePlaceHolder").innerHTML = data.name;
+      document.getElementById("userPointsPlaceHolder").innerHTML = data.points;
+      document.getElementById("userPredictionsMadePlaceHolder").innerHTML =
+        data.predictionsMade;
+      document.getElementById("userJoinDatePlaceHolder").innerHTML =
+        data.timestamp.toDate().toDateString();
 
-const docRef = doc(db, "users", token);
-const docSnap = await getDoc(docRef);
+      const pastPredictionsHolder = document.getElementById(
+        "past-predictions-holder"
+      );
 
-if (docSnap.exists()) {
-//   console.log("Document data:", docSnap.data());
-    const target = docSnap.data();
-    document.getElementById("userRankPlaceHolder").innerHTML
-        = target.leaderboardPos;
-    document.getElementById("userNamePlaceHolder").innerHTML
-        = target.name;
-    document.getElementById("userPointsPlaceHolder").innerHTML
-        = target.points;
-    document.getElementById("userPredictionsMadePlaceHolder").innerHTML
-        = target.predictionsMade;
-    document.getElementById("userJoinDatePlaceHolder").innerHTML
-        = target.timestamp;
-} else {
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
+      console.log(Object.entries(data.predictions))
+
+      Object.entries(data.predictions).forEach(([name, country]) => {
+        const template = document.getElementById("past-predictions-template");
+
+        const card = template.content.cloneNode(true);
+
+        card.querySelectorAll("h3")[0].innerText = name;
+        card.querySelectorAll("p")[0].innerText = country;
+
+        pastPredictionsHolder.appendChild(card);
+      });
+    } else {
+      console.log("No such document!");
+    }
+  });
 }
+
+main();
