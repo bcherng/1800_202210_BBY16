6e0be7b6564b86bd2ec7764b70255ee218f6f03c
Simrat Grewal <simrats169169@gmail.com>
2022-03-30 20:31:23 -0700

    feat(redeem-store): Update points on redeem and show popup with code

--- scripts/redeem-store.js
+++ scripts/redeem-store.js
@@ -1,47 +1,73 @@
 import { db } from "./firebaseAPI_TEAM_BBY_16.js";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { getID } from "./index/login.js";
 
-const token = window.localStorage.getItem("token");
+let userPoints = 0;
 
-const docRef1 = doc(db, "users", token);
-const docSnap = await getDoc(docRef1);
-console.log(docSnap);
+setUserPoints().then(() => {
+  setCards()
+})
 
-if (docSnap.exists()) {
-  const target = docSnap.data().points;
-  console.log(target);
-  document.getElementById("PointsPlaceHolder").innerHTML = target;
-} else {
-  console.log("No such document!");
-}
+async function setUserPoints() {
+  const token = window.localStorage.getItem("token");
 
-function pointChecker() {
-  if (target <= 0) {
-    console.log("Not enough points");
+  const docRef1 = doc(db, "users", token);
+  const docSnap = await getDoc(docRef1);
+
+  if (docSnap.exists()) {
+    const target = docSnap.data().points;
+    userPoints = parseInt(target);
+    document.getElementById("PointsPlaceHolder").innerHTML = target;
   } else {
-    console.log("Redeem sucessful");
-    target -= 5;
+    console.log("No such document!");
   }
 }
 
-const docRef2 = doc(db, "Rewards", "giftCards");
-const docSnap2 = await getDoc(docRef2);
+async function setCards() {
+  const docRef2 = doc(db, "Rewards", "giftCards");
+  const docSnap2 = await getDoc(docRef2);
 
-// Adds info such as name, point and image.
-if (docSnap2.exists()) {
-  const target2 = docSnap2.data();
-  console.log(target2);
-  var i = 1;
-  target2.cards.forEach((reward) => {
-    let option = document.getElementById(`option${i}`);
-    let points = document.getElementById(`point${i}`);
-    let img = document.getElementById(`image${i}`);
+  // Adds info such as name, point and image.
+  if (docSnap2.exists()) {
+    const target2 = docSnap2.data();
 
-    img.setAttribute("src", reward.img);
+    var i = 1;
 
-    points.innerHTML = reward.points + " points";
-    option.innerHTML = reward.name;
+    target2.cards.forEach((reward) => {
+      let option = document.getElementById(`option${i}`);
+      let points = document.getElementById(`point${i}`);
+      let img = document.getElementById(`image${i}`);
 
-    i++;
-  });
+      let btn = document.getElementById(`btn${i}`);
+
+      if (userPoints < reward.points) {
+        btn.disabled = true;
+      } else {
+        btn.addEventListener('click', () => {
+          alert("Redeemed Code: 84W2yNB6")
+          btn.disabled = true;
+
+          let userDoc = doc(db, `users/${getID()}`)
+
+          getDoc(userDoc).then((snap) => {
+            let newPoints = snap.data().points - parseInt(reward.points)
+            updateDoc(userDoc, {
+              points: newPoints,
+            }).then(() => {
+              setUserPoints().then(() => {
+                setCards()
+              })
+            })
+          })
+        })
+      }
+      
+      img.setAttribute("src", reward.img);
+
+      points.innerHTML = reward.points + " points";
+      option.innerHTML = reward.name;
+
+      i++;
+    });
+  }
 }
