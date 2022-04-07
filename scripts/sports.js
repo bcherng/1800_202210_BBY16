462c7f57984c5cb6d16d5cae57bdaaaadab092e6
Simrat Grewal <simrats169169@gmail.com>
2022-04-07 14:20:37 -0700

    sports: Add banners to firebase

--- scripts/sports.js
+++ scripts/sports.js
@@ -151,6 +151,11 @@ function writeSportsData() {
     Option2: "Austria",
     Option3: "Norway",
     Option4: "Switzerland",
+    banners: [
+      "https://cloudfront-us-east-2.images.arcpublishing.com/reuters/SSLLVRFNJVNLTLGVDUC6KF4P3E.jpg",
+      "https://i.eurosport.com/2022/03/17/3339270-68265668-2560-1440.jpg",
+      "https://onherturf.nbcsports.com/wp-content/uploads/sites/26/2022/02/GettyImages-1370428646-e1644888347757.jpg?w=1013"
+    ],
   });
 
   addDoc(ref, {
@@ -163,6 +168,11 @@ function writeSportsData() {
     Option2: "Norway",
     Option3: "Italy",
     Option4: "Germany",
+    banners: [
+      "https://media.self.com/photos/61fac72782bc34bc3352c7ca/4:3/w_2560%2Cc_limit/GettyImages-1238112330.jpg",
+      "https://www.gannett-cdn.com/-mm-/091190c344f99fabe8e83297172ad0de8a040519/c=0-156-3072-1892/local/-/media/2018/01/30/USATODAY/USATODAY/636529149648182788-EPA-ITALY-BIATHLON-WORLD-CUP-96769415.JPG?width=3072&height=1736&fit=crop&format=pjpg&auto=webp",
+      "https://canmorealberta.com/assets/images/canmore-stories/1935/_1200x630_crop_center-center_82_none/Biathlon.Mens.1.jpg?mtime=1548865014"
+    ],
   });
 
   addDoc(ref, {
@@ -175,6 +185,11 @@ function writeSportsData() {
     Option2: "Germany",
     Option3: "Republic of Korea",
     Option4: "Switzerland",
+    banners: [
+      "https://upload.wikimedia.org/wikipedia/commons/6/64/USA_I_in_heat_1_of_2_man_bobsleigh_at_2010_Winter_Olympics_2010-02-20.jpg",
+      "https://brocku.ca/brock-news/wp-content/uploads/2020/12/BobsledHaughey-1600x1290.jpg?x70330",
+      "https://www.sportsnet.ca/wp-content/uploads/2022/02/Bobsled-1040x572.jpg"
+    ],
   });
 
   addDoc(ref, {
@@ -187,6 +202,11 @@ function writeSportsData() {
     Option2: "Sweden",
     Option3: "Fnland",
     Option4: "USA",
+    banners: [
+      "https://www.rei.com/dam/kingman_120519_0459_classic_skiing.jpg",
+      "https://www.rossignol.com/media/wysiwyg/-1-GROOMED-TRAIL_1.jpg",
+      "http://cdn.shopify.com/s/files/1/1409/1390/articles/pmx010118-skiing1912-1607384267.jpg?v=1618604395"
+    ],
   });
 
   addDoc(ref, {
@@ -199,6 +219,11 @@ function writeSportsData() {
     Option2: "Sweden",
     Option3: "Switzerland",
     Option4: "Canada",
+    banners: [
+      "https://img.olympicchannel.com/images/image/private/t_16-9_360-203_2x/f_auto/v1538355600/primary/w5z6bngcm13t9vqugoja",
+      "https://i.cbc.ca/1.6392864.1647914240!/cpImage/httpImage/image.jpg_gen/derivatives/16x9_780/gushue-gallant-130322.jpg",
+      "https://img.olympicchannel.com/images/image/private/t_16-9_3200/primary/jwo3as3xzvetfwvje3yo"
+    ],
   });
 
   addDoc(ref, {
@@ -211,6 +236,11 @@ function writeSportsData() {
     Option2: "France",
     Option3: "USA",
     Option4: "Canada",
+    banners: [
+      "https://www.atlanticcouncil.org/wp-content/uploads/2022/02/2022-02-17T150901Z_337896894_SP1EI2H162XLT_RTRMADP_3_OLYMPICS-2022-FIGURESKATING-scaled-e1645113703304.jpg",
+      "https://i.cbc.ca/1.6392034.1647875341!/fileImage/httpImage/1370429737.jpg",
+      "https://img.olympicchannel.com/images/image/private/t_16-9_360-203_2x/f_auto/v1538355600/primary/sp0oqmdagaf6i4obduta"      
+    ],
   });
 
   addDoc(ref, {
@@ -223,6 +253,11 @@ function writeSportsData() {
     Option2: "China",
     Option3: "Australia",
     Option4: "USA",
+    banners: [
+      "https://i.cbc.ca/1.6392034.1647875341!/fileImage/httpImage/1370429737.jpg",
+      "https://img.olympicchannel.com/images/image/private/t_16-9_3200/primary/vmgsnuprpa1erxtvkl5y",
+      "https://cdn.vox-cdn.com/thumbor/pDoARY33YNRf3ZMRl91ND0uaOsw=/0x0:3804x2693/1400x933/filters:focal(1468x1116:2076x1724):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/70508661/1238474138.0.jpg"
+    ],
   });
 
   addDoc(ref, {
@@ -235,6 +270,11 @@ function writeSportsData() {
     Option2: "Germany",
     Option3: "Canada ",
     Option4: "Czech Republic",
+    banners: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Capitals-Maple_Leafs_%2834075134291%29.jpg/1200px-Capitals-Maple_Leafs_%2834075134291%29.jpg",
+      "https://cloudfront-us-east-2.images.arcpublishing.com/reuters/62PSK6LQPJHSLESKX7366VR5NM.JPG",
+      "https://img.redbull.com/images/c_crop,x_127,y_0,h_3712,w_4124/c_fill,w_860,h_782/q_auto,f_auto/redbullcom/2018/07/06/ecbc1531-9474-4153-b3b2-c54a0a3dd81e/ice-hockey-collection"
+    ],
   });
 
   addDoc(ref, {
@@ -247,6 +287,11 @@ function writeSportsData() {
     Option2: "Austria",
     Option3: "Canada",
     Option4: "Latvia",
+    banners: [
+      "https://www.cbc.ca/kids/images/beijing2022_luge_header.jpg",
+      "https://s.abcnews.com/images/Sports/WireAP_30886c12202042fbb78e16ffe4d05986_16x9_1600.jpg",
+      "https://img.olympicchannel.com/images/image/private/t_16-9_360-203_2x/f_auto/v1538355600/primary/r21h2japzjujezdkfuic"
+    ],
   });
 
   addDoc(ref, {
@@ -259,6 +304,11 @@ function writeSportsData() {
     Option2: "Norway",
     Option3: "Japan",
     Option4: "Finland",
+    banners: [
+      "https://images.nbcolympics.com/sites/default/files/2021-10/nordic-glossary-thumb.jpg",
+      "https://ftw.usatoday.com/wp-content/uploads/sites/90/2022/01/AP-US-Olympic-Trials-Nordic-Combined-Skiing.jpg?w=1000&h=600&crop=1",
+      "https://usskiandsnowboard.org/sites/default/files/images/static-pages/StaticPageHeader_1600x1200_Nordic_Combined_0.jpg"
+    ],
   });
 
   addDoc(ref, {
@@ -271,6 +321,11 @@ function writeSportsData() {
     Option2: "Canada",
     Option3: "Italy",
     Option4: "Korea",
+    banners: [
+      "https://www.cbc.ca/kids/images/beijing2022_short_track_header.jpg",
+      "https://olympic.ca/wp-content/uploads/2019/11/20039406.jpg?quality=100",
+      "https://static01.nyt.com/images/2014/02/16/sports/SUB-SHORTTRACK/SUB-SHORTTRACK-superJumbo.jpg"
+    ],
   });
 
   addDoc(ref, {
@@ -283,6 +338,11 @@ function writeSportsData() {
     Option2: "Olympic Athletes from Russia",
     Option3: "Great Britain",
     Option4: "Latvia",
+    banners: [
+      "https://olympic.ca/wp-content/uploads/2020/05/Jon-Montgomery.jpg?quality=100",
+      "https://olympic.ca/wp-content/uploads/2020/10/Team-Canada-Mirela-Rahneva-e1603223924189.jpg?quality=100",
+      "https://onherturf.nbcsports.com/wp-content/uploads/sites/26/2022/02/GettyImages-1238313894-e1644478876483.jpg?w=1024"
+    ],
   });
 
   addDoc(ref, {
@@ -295,6 +355,11 @@ function writeSportsData() {
     Option2: "Germany",
     Option3: "Japan",
     Option4: "Olympic Athletes from Russia",
+    banners: [
+      "https://upload.wikimedia.org/wikipedia/commons/b/b9/FIS_Ski_Weltcup_Titisee-Neustadt_2016_-_Peter_Prevc1.jpg",
+      "https://images.theconversation.com/files/445195/original/file-20220208-21-nw1u31.jpg?ixlib=rb-1.1.0&rect=0%2C319%2C3751%2C1875&q=45&auto=format&w=1356&h=668&fit=crop",
+      "https://i.insider.com/5a7dce54d030720a038b4c09?width=700"
+    ],
   });
 
   addDoc(ref, {
@@ -307,6 +372,11 @@ function writeSportsData() {
     Option2: "USA",
     Option3: "New Zealand",
     Option4: "Japan",
+    banners: [
+      "https://www.rei.com/dam/content_team_082817_18617_training_for_snowboarding.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/1/16/Snowboarding.jpg",
+      "https://www.snowskool.com/uploads/images/Intermediate_to_Advanced_Boarding.jpg"
+    ],
   });
 
   addDoc(ref, {
@@ -319,5 +389,12 @@ function writeSportsData() {
     Option2: "Japan",
     Option3: "USA",
     Option4: "Austria",
+    banners: [
+      "https://img.olympicchannel.com/images/image/private/t_16-9_3200/primary/vyfp4pntaxoyf3uruhpu",
+      "https://olympic.ca/wp-content/uploads/2019/11/20039406.jpg?quality=100",
+      "https://cdn.vox-cdn.com/thumbor/47Nihv_olkYwcX3k44C71FES8nQ=/0x0:5190x3300/1400x933/filters:focal(2143x775:2973x1605):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/70494969/1369702614.0.jpg"
+    ],
   });
 }
+
+// writeSportsData()
